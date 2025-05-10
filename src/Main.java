c3f47c9e3bc8ce76ef6704c0dfc4fe690616cf8b
malkov <malkov922522@gmail.com>
2025-05-10 13:16:08 +0400

    commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 250;
+        int upAmmount = 900;
+
+        int bonus;
+        if (upAmmount > 1000) {
+            bonus = upAmmount / 100;
+        } else {
+            bonus = 0;
+            }
+
+        int totalBalance = balance + upAmmount + bonus;
+
+        System.out.println("Текущий баланс: " + balance + " руб.");
+        System.out.println("Сумма пополнения: " + upAmmount + " руб.");
+        System.out.println("Начислено бонусных рублей: " + bonus + " руб.");
+        System.out.println("Итоговый счёт: " + totalBalance + " руб.");
+
+
+
+
+
+
+
+    }
+    }
